efa994d4087223908210337083e80eca1afc265d
Adriano Oliveira <a.oliveira@kuehne-nagel.com>
2022-12-16 19:53:43 +0000

    added interview functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import { Button, Card, CardContent, Typography } from '@mui/material';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Button,
+  Card,
+  CardContent,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
 
-import { babyNames } from './utils/babyNames';
-import { partition } from './utils/utils';
+import { partition } from "./utils/utils";
 
 const GENDERS = {
   MALE: 0,
@@ -44,23 +51,76 @@ function pickRandomName(popularityMeter: { [key: number]: string }) {
 }
 
 function App() {
-  const dataset = babyNames;
+  const [selectedGender, setSelectedGender] = useState(GENDERS.MALE);
 
-  const [currentName, setCurrentName] = useState('Select a gender');
+  const [popularityMeters, setPopularityMeters] = useState<
+    { [key: number]: string }[]
+  >([]);
 
-  const datasets = partition(dataset, (elem: string[]) => elem[1] === 'MALE');
+  const [namesByEthnicity, setNamesByEthnicity] = useState<{
+    [key: string]: string[][];
+  }>({});
 
-  let popularityMeterMale: { [key: number]: string } = {};
-  let popularityMeterFemale: { [key: number]: string } = {};
+  const [selectedEthnicity, setSelectedEthnicity] =
+    useState<string>("HISPANIC");
 
-  popularityMeterMale = fillPopularityMeter(datasets[0]);
-  popularityMeterFemale = fillPopularityMeter(datasets[1]);
+  const [ethnicities, setEthnicities] = useState<string[]>([]);
 
-  const popularityMeters = [popularityMeterMale, popularityMeterFemale];
+  const [currentName, setCurrentName] = useState("");
 
-  function handleOnClick(gender: number) {
-    const name = pickRandomName(popularityMeters[gender]);
+  useEffect(() => {
+    fetch("https://data.cityofnewyork.us/api/views/25th-nujf/rows.json")
+      .then((res) => res.json())
+      .then((response) => {
+        const dataset = response.data;
+
+        const mappedData = dataset.map((elem: string[]) => elem.slice(8));
+
+        const namesByEthnicity: { [key: string]: string[][] } = {};
+
+        for (const entry of mappedData) {
+          if (namesByEthnicity[entry[2]]) {
+            namesByEthnicity[entry[2]].push(entry);
+          } else {
+            namesByEthnicity[entry[2]] = [entry];
+          }
+        }
+
+        setNamesByEthnicity(namesByEthnicity);
+
+        setEthnicities(Object.keys(namesByEthnicity));
+      });
+  }, []);
+
+  function handleOnSelectGender(gender: number) {
+    setSelectedGender(gender);
+  }
+
+  function handleOnGenerateName() {
+    const name = pickRandomName(popularityMeters[selectedGender]);
     setCurrentName(name);
+  }
+
+  function fillPopMeters(ethnicity: string) {
+
+
+    const datasets = partition(
+      namesByEthnicity[ethnicity],
+      (elem: string[]) => elem[1] === "MALE"
+    );
+
+    let popularityMeterMale: { [key: number]: string } = {};
+    let popularityMeterFemale: { [key: number]: string } = {};
+
+    popularityMeterMale = fillPopularityMeter(datasets[0]);
+    popularityMeterFemale = fillPopularityMeter(datasets[1]);
+
+    setPopularityMeters([popularityMeterMale, popularityMeterFemale]);
+  }
+
+  function handleDropdownChange(event: SelectChangeEvent<string>) {
+    setSelectedEthnicity(event.target.value);
+    fillPopMeters(event.target.value)
   }
 
   return (
@@ -69,23 +129,42 @@ function App() {
       <div className="Container">
         <div className="Buttons">
           <Button
-            color="primary"
+            color={selectedGender === GENDERS.MALE ? "success" : "primary"}
             size="large"
             variant="outlined"
-            onClick={() => handleOnClick(GENDERS.MALE)}
+            onClick={() => handleOnSelectGender(GENDERS.MALE)}
           >
             Male
           </Button>
           <Button
-            color="primary"
             size="large"
+            color={selectedGender === GENDERS.FEMALE ? "success" : "primary"}
             variant="outlined"
-            onClick={() => handleOnClick(GENDERS.FEMALE)}
+            onClick={() => handleOnSelectGender(GENDERS.FEMALE)}
           >
             Female
           </Button>
         </div>
         <div className="Break"></div>
+        <Select value={selectedEthnicity} onChange={handleDropdownChange}>
+          {ethnicities.map((ethnicity) => (
+            <MenuItem key={ethnicity} value={ethnicity}>
+              {ethnicity}
+            </MenuItem>
+          ))}
+        </Select>
+        <div className="Break"></div>
+
+        <Button
+          color="primary"
+          size="large"
+          variant="outlined"
+          onClick={() => handleOnGenerateName()}
+        >
+          Generate
+        </Button>
+        <div className="Break"></div>
+
         <Card>
           <CardContent>
             <Typography variant="h4">
