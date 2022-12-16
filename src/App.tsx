9215fdaac92f66675b14196049427232288d6433
Minskyy <adriano.oliveira@ua.pt>
2022-12-16 00:00:56 +0000

    woeking app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+} from '@mui/material';
+
+import { babyNames } from './utils/babyNames';
+import { partition } from './utils/utils';
+
+const GENDERS = {
+  MALE: 0,
+  FEMALE: 1,
+};
 
 function App() {
+  const dataset = babyNames;
+
+  const [currentName, setCurrentName] = useState('Select a gender');
+
+  const datasets = partition(dataset, (elem: string[]) => elem[1] === 'MALE');
+
+  function pickRandomName(gender: number) {
+    // Typescript gives us an error on trying to access with .length, so this workaround was made
+    const index = Math.floor(Math.random() * datasets[gender]['length']);
+
+    const [YoB, babyGender, ethnicity, name, equalNameCount, rank] =
+      datasets[gender][index];
+
+    setCurrentName(name);
+
+    return datasets[gender][index];
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">Baby name generator</div>
+      <div className="Container">
+        <div className="Buttons">
+          <Button
+            color="primary"
+            size="large"
+            variant="outlined"
+            onClick={() => pickRandomName(GENDERS.MALE)}
+          >
+            Male
+          </Button>
+          <Button
+            color="primary"
+            size="large"
+            variant="outlined"
+            onClick={() => pickRandomName(GENDERS.FEMALE)}
+          >
+            Female
+          </Button>
+        </div>
+        <div className="Break"></div>
+        <Card>
+          <CardContent>
+            <Typography variant="h4">
+              {/* names are coming both in upper and lower case. 
+            So we format to title case by setting to lower case here, and then capitalizing with css */}
+              {currentName.toLowerCase()}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,1 @@
+export const partition = (arr, criteria) => arr.reduce((acc, i) => (acc[criteria(i) ? 0 : 1].push(i), acc), [[], []]);
